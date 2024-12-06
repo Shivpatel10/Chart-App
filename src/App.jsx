69b219ec8d1318275bb7520e4433d3294b7a447e
Shiv Patel <shivpatel0204@gmail.com>
2024-12-05 19:46:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import BarChart from 'chartjs';
+import './App.css'
+
+function App() {
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => response.json())
+      .then((data) => setChartData(data));
+    }, []);
+  
+    if (!chartData) {
+      return <div>Loading...</div>;
+    }
+    
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Dynamic Charts with React and Chart.js</h1>
+        <BarChart data={chartData} />
+      </div>
+    );
+  }
+    
+export default App
